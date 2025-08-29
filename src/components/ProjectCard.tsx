d46ef80d6b793b21f1d1bd1bf07f3fa776865a8f
Baptiste Arnaudo <baptiste.arnaudo@polytechnique.edu>
2025-08-29 09:51:19 +0200

    Changed logo size and added ge

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -18,7 +18,7 @@ const ProjectCard = ({ title, description, keywords, codeUrl, pdfUrl, logo }: Pr
           <img 
             src={logo} 
             alt={`${title} logo`} 
-            className="w-15 h-15 object-contain"
+            className="w-12 h-12 object-contain"
           />
         )}
         <h3 className="project-title">{title}</h3>
